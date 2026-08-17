71f70c975dc74b4243d9f8686f1d7be11e493e95
Javier Ibarra <ibarra.161804@e.unavarra.es>
2026-08-17 21:46:35 +0200

    Change version buttons to Dialga/Palkia icons

--- src/App.jsx
+++ src/App.jsx
@@ -620,8 +620,12 @@ export default function App() {
           </div>
 
           <div className="buttons">
-            <button className={`action-button button-b ${version === 'pearl' ? 'active' : ''}`} onClick={() => setVersion('pearl')} title="Shining Pearl">B</button>
-            <button className={`action-button button-a ${version === 'diamond' ? 'active' : ''}`} onClick={() => setVersion('diamond')} title="Brilliant Diamond">A</button>
+            <button className={`action-button button-b ${version === 'pearl' ? 'active' : ''}`} onClick={() => setVersion('pearl')} title="Shining Pearl">
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/484.png" alt="Palkia" style={{width: '35px', height: '35px'}} />
+            </button>
+            <button className={`action-button button-a ${version === 'diamond' ? 'active' : ''}`} onClick={() => setVersion('diamond')} title="Brilliant Diamond">
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/483.png" alt="Dialga" style={{width: '35px', height: '35px'}} />
+            </button>
           </div>
         </div>
       </div>
